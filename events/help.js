1eb3fa8ff60539b8c07dca28400245b35ea03dc2
miles20232131231 <gamingrobloxmilesplayz@outlook.com>
2024-09-14 21:27:00 +0200

    Update help.js

--- events/help.js
+++ events/help.js
@@ -18,21 +18,21 @@ async function handleHelpDropdown(interaction) {
         case 'register_vehicle':
             embed = new EmbedBuilder()
                 .setTitle('How can I Register my Vehicle?')
-                .setDescription('To register your vehicle, use our Discord Bot and head over to the <#1264026498373783562> channel. You can register up to 8 vehicles if you’re a booster, and up to 3 vehicles as a regular user.')
+                .setDescription('To register your vehicle, use our Discord Bot and head over to the <#1280229550831435827> channel. You can register up to 8 vehicles if you’re a booster, and up to 3 vehicles as a regular user.')
                 .setColor('#3498db');
             break;
 
         case 'join_swfl_session':
             embed = new EmbedBuilder()
                 .setTitle('How do I join SWFL Session?')
-                .setDescription('To join our Southwest Florida Roleplay sessions, watch for pings in <#1264026944299728906>. You can also use the reaction roles to set notifications for session pings.')
+                .setDescription('To join our Southwest Florida Roleplay sessions, watch for pings in https://discord.com/channels/1277470399952850995/1282100737274413137/1284362314346401832. You can also use the reaction roles to set notifications for session pings.')
                 .setColor('#3498db');
             break;
 
         case 'become_staff_ps_member':
             embed = new EmbedBuilder()
                 .setTitle('How can I be a Staff or P/S Member?')
-                .setDescription('To become a Staff or P/S Member, check out the <#1253579753621950546> channel and look for the applications available via dropdown.')
+                .setDescription('To become a Staff or P/S Member, check out the https://discord.com/channels/1277470399952850995/1282100448836452414/1284312902219599873 channel and look for the applications available via dropdown.')
                 .setColor('#3498db');
             break;
 
